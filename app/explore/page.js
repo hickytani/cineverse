cb6fddb30707759927d407f65a23c2154b1a02ad
hickytani <prasun051205@gmail.com>
2026-04-08 23:08:51 +0530

    fix: nuclear suspense fix

---
+++ app/explore/page.js
@@ -0,0 +1,5 @@
+'use client';
+import { Suspense } from 'react';
+
+export default function Page() { return <Suspense fallback={<div>Loading...</div>}><ExplorePage /></Suspense> };
+export const dynamic = 'force-dynamic';
