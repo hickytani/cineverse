117e073f1d6a2279183ee57bc3c0aca597355eac
hickytani <prasun051205@gmail.com>
2026-04-08 23:05:35 +0530

    fix: force dynamic and suspense

--- app/explore/page.js
+++ app/explore/page.js
@@ -1,2 +1,4 @@
+'use client';
 import { Suspense } from 'react';
-export default function Page() { return <Suspense fallback={<div>Loading...</div>}><Page /></Suspense> }
+ export default function Page() { return <Suspense fallback={<div>Loading...</div>}><Page /></Suspense> }
+export const dynamic = 'force-dynamic';
