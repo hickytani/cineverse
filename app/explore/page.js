601222b6cb483760755943cdbbac88d0e3bc9d0a
hickytani <prasun051205@gmail.com>
2026-04-08 23:05:45 +0530

    fix: remove duplicate js file

--- app/explore/page.js
+++
@@ -1,4 +0,0 @@
-'use client';
-import { Suspense } from 'react';
- export default function Page() { return <Suspense fallback={<div>Loading...</div>}><Page /></Suspense> }
-export const dynamic = 'force-dynamic';
