49350d869e5e2a5e1f00247bfcd3b1e753ee502b
hickytani <prasun051205@gmail.com>
2026-04-08 23:02:23 +0530

    final fix

--- app/explore/page.js
+++ app/explore/page.js
@@ -1,2 +1,2 @@
 import { Suspense } from 'react';
-export default function Page() { return <Suspense fallback={<div>Loading...</div>}><ExplorePage /></Suspense> }
+export default function Page() { return <Suspense fallback={<div>Loading...</div>}><Page /></Suspense> }
